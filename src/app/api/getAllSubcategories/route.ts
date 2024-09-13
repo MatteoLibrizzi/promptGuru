40b50d4f98fc0885b628b4e59ea35c72135d68ad
Matteo Librizzi <mttlbr@amazon.lu>
2024-09-13 22:33:25 +0200

    Removing default in library route

--- src/app/api/getAllSubcategories/route.ts
+++ src/app/api/getAllSubcategories/route.ts
@@ -1,6 +1,6 @@
 import { HardcodedPromptCategoriesRepository } from "@/app/api/repositories/promptCategories"
 
-export default async function GET(request: Request) {
+export async function GET(request: Request) {
 
     const promptsRepository = new HardcodedPromptCategoriesRepository()
 
